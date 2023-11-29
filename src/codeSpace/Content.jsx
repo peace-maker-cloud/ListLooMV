834af900d68719961bf4df4f3bd11cc088fb6241
Vigneshwaran Ravi <110144091+peace-maker-cloud@users.noreply.github.com>
2023-11-29 13:48:54 +0530

    updated ROuter hook

--- src/codeSpace/Content.jsx
+++ src/codeSpace/Content.jsx
@@ -1,26 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Listwork } from "./workPage/listwork";
 import { Taskwork } from "./workPage/taskwork";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
-export const Content = ({
-  userTask,
-  fetchErr,
-  isLoading,
-  go,
-  checks,
-  deletes,
-}) => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (go === "List") {
-      navigate("/listwork");
-    } else {
-      navigate("/taskwork");
-    }
-  }, [go, navigate]);
-
+export const Content = ({ userTask, fetchErr, isLoading, checks, deletes }) => {
   return (
     <div className="mx-auto flex flex-col justify-between">
       <div>
